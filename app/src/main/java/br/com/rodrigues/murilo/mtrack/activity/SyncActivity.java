26ed0ce00db9d97e47739657556bee29266cbbe1
Murilo Rodrigues <murilorodrigues62@gmail.com>
2017-03-01 17:03:37 -0300

    change the sdk to 25

--- app/src/main/java/br/com/rodrigues/murilo/mtrack/activity/SyncActivity.java
+++ app/src/main/java/br/com/rodrigues/murilo/mtrack/activity/SyncActivity.java
@@ -1,14 +1,14 @@
 package br.com.rodrigues.murilo.mtrack.activity;
-
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import br.com.rodrigues.murilo.mtrack.R;
 import br.com.rodrigues.murilo.mtrack.ui.base.BaseActivity;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -16,6 +16,10 @@ import butterknife.OnClick;
  * System Settings
  */
 public class SyncActivity extends BaseActivity {
+    private ProgressBar progress;
+
+    @Bind(R.id.progressBar)
+    public ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,8 @@ public class SyncActivity extends BaseActivity {
 
     @OnClick(R.id.fab)
     public void onFabClicked(View view) {
-        Snackbar.make(view, R.string.message_saved, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+       // Snackbar.make(view, R.string.message_saved, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        sync(view);
     }
 
     private void setupToolbar() {
@@ -61,5 +66,29 @@ public class SyncActivity extends BaseActivity {
     public boolean providesActivityToolbar() {
         return true;
     }
-    // TODO: 23/02/17 continuar 
+    // TODO: 23/02/17 continuar
+
+    public void sync(View view){
+        // TODO: 01/03/17 impĺementar progressbar 
+
+        final int totalProgressTime = 100;
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                int jumpTime = 0;
+
+                while(jumpTime < totalProgressTime) {
+                    try {
+                        sleep(200);
+                        jumpTime += 5;
+                        progress.setProgress(jumpTime);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
 }
